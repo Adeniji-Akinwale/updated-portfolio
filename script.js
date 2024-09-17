d0eb17de557f5730bb9862b26acbc7797d588d9d
Adeniji Wareez Akinwale <99355374+Adeniji-Akinwale@users.noreply.github.com>
2024-09-17 05:05:05 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// const hamburgerMenu = document.getElementById('hamburgerMenu');
+// const navBar = document.getElementById('navBar');
+
+// hamburgerMenu.addEventListener('click', () => {
+//   navBar.classList.toggle('nav-active');
+// });
